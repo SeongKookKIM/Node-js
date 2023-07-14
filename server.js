48c8254367d29c123004a5b85d7043741a48c72f
SeongkookKIM <ksk8646@naver.com>
2023-07-14 16:56:11 +0900

    AJAX로 삭제요청하기 3 (jQuery를 이용한 UI 기능)

--- server.js
+++ server.js
@@ -102,5 +102,6 @@ app.delete("/delete", (요청, 응답) => {
   요청.body._id = parseInt(요청.body._id);
   db.collection("post").deleteOne(요청.body, function (에러, 결과) {
     console.log("삭제완료");
+    응답.status(200).send({ message: "성공했습니다" });
   });
 });
