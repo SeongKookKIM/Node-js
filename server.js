6d4276a6f28e2256c3f8b3172645ff925b98a788
SeongkookKIM <ksk8646@naver.com>
2023-07-14 10:36:13 +0900

    POST 요청법

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+// npm install body-parser할 경우
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// 이 코드가 있어야 req.body가능
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
   console.log("listening on 8080");
@@ -13,11 +19,17 @@ app.get("/beauty", function (req, res) {
   res.send("뷰티용품 사세요!");
 });
 
-// html 보내기
-app.get("/", function (req, res) {
+// html 보내기(GET)
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/write", function (req, res) {
+app.get("/write", (req, res) => {
   res.sendFile(__dirname + "/write.html");
+});
+
+// POST
+app.post("/add", (req, res) => {
+  res.send("전송완료");
+  console.log(req.body);
 });
