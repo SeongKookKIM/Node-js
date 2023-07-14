1ea58b6871af346079f3b5b0c0c1f11419f41ce6
SeongkookKIM <ksk8646@naver.com>
2023-07-14 12:58:18 +0900

    /add POST시 DB에 내용 저장

--- server.js
+++ server.js
@@ -8,15 +8,34 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB
+var db;
 const MongoClient = require("mongodb").MongoClient;
 MongoClient.connect(
   "mongodb+srv://admin:tlqkfdk2@@cluster0.n6phqup.mongodb.net/?retryWrites=true&w=majority",
   (에러, client) => {
-    app.listen(8080, function () {
-      console.log("listening on 8080");
+    if (에러) {
+      return console.log(에러);
+    }
+    db = client.db("todoapp");
+
+    // db.collection("post").insertOne(
+    //   { name: "Sam", age: 28, _id: 100 },
+    //   (에러, 결과) => {
+    //     console.log("저장완료");
+    //   }
+    // );
+    app.post("/add", (req, res) => {
+      res.send("전송완료");
+      db.collection("post").insertOne(req.body, (err, result) => {
+        console.log("저장완료");
+      });
     });
   }
 );
+
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
 
 app.get("/pet", function (req, res) {
   res.send("펫용품 쇼핑 사이트입니다.");
@@ -36,7 +55,7 @@ app.get("/write", (req, res) => {
 });
 
 // POST
-app.post("/add", (req, res) => {
-  res.send("전송완료");
-  console.log(req.body);
-});
+// app.post("/add", (req, res) => {
+//   res.send("전송완료");
+//   console.log(req.body);
+// });
